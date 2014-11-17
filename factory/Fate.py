e29a3ba10a14ae4c9a52b411bf7a30e681127fee
kevgao <kevgao@live.com>
2014-11-16 17:07:13 -0700

    Setup code for factory pattern

---
+++ factory/Fate.py
@@ -0,0 +1,6 @@
+import Servant
+factory=Servant.ServantFactory()
+Saber=factory.getServant("Saber")
+Archer=factory.getServant("Assassin")
+Rider=factory.getServant("Rider")
+Berserker=factory.getServant("Berserker")
---
+++ factory/Servant.py
@@ -0,0 +1,35 @@
+class Saber:
+    def __init__(self):
+        self.name="King Arthur"
+        self.gender="Female"
+        self.armor="Excalibur"
+        self.country="Britain"
+        print "Saber comes."
+
+class Rider:
+    def __init__(self):
+        self.name="Alexander the Great"
+        self.gender="Male"
+        self.armor="Kupriotes"
+        self.country="Macedon"
+        print "Rider comes"
+
+class Assassin:
+    def __init__(self):
+        self.name="Hassan-i Sabbah"
+        self.gender="Male"
+        self.armor=None
+        self.country="Persia"
+        print "Assassin comes"
+
+class Berserker:
+    def __init__(self):
+        self.name="Lancelot"
+        self.gender="Male"
+        self.armor=None
+        self.country="France"
+        print "Berserker comes"
+
+class ServantFactory:
+    pass
+
