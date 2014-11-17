b847d7c9a17c8859b4a17533b585dfff81bd2099
Duanfeng Gao <kevgao@live.com>
2014-11-16 17:17:41 -0700

    Delete Fate.py

--- factory/Fate.py
+++
@@ -1,6 +0,0 @@
-import Servant
-factory=Servant.ServantFactory()
-Saber=factory.getServant("Saber")
-Archer=factory.getServant("Assassin")
-Rider=factory.getServant("Rider")
-Berserker=factory.getServant("Berserker")
